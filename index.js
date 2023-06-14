ec6d46c9a2fca9d8113fb031095412d7a0483354
CAROLKOECH <koechc44@gmail.com>
2023-06-14 14:03:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+// Define the 'currentUser' variable
+const currentUser = 'John Smith';
+
+// Define the 'welcomeMessage' variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the 'excitedWelcomeMessage' variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the 'shortGreeting' variable
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+// Export the variables (if needed)
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting
+};
